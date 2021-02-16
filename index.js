3994a9cb48ae9ccc33b34197412ff7284251de5b
william <masterpkpk@gmail.com>
2021-02-16 12:57:17 -0500

    commit tree c6fdbf00817bcac0865165ee4516a3e2cb257dda

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+  let thanks = []
+  for (let i = 0; i < array.length; i++ ) {
+  thanks.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+  return thanks
+}
+
+function countDown(integer) {
+  let i = integer
+  while (i >= 0) {
+    console.log(i)
+    i--
+  }
+}
